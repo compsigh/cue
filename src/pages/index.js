80bd8fb377556d274aa942bbf5dd5940a09640b7
Sanjana Prasad <sprasad10@dons.usfca.edu>
2023-03-31 20:49:45 -0700

    add update splashscreen

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,22 +16,18 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
           <div>
             <a
               href="https://cue.study"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              {}
               <Image
                 src="/cue.svg"
                 alt="Cue Logo"
                 className={styles.vercelLogo}
-                width={100}
+                width={25}
                 height={24}
                 priority
               />
@@ -40,84 +36,28 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
+        <h2 className={patrickHand.className}>
+              Cue 
+            </h2>
+            <hr/>
+        <div className={styles.thirteen}>
+        <a href="https://cue.study" className={styles.button}>
+
           <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
+            src="/cue.svg"
+            alt="Cue Logo"
+            width={90}
+            height={90}
             priority
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={patrickHand.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={patrickHand.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={patrickHand.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={patrickHand.className}>
-              Learn about Next.js in an patrickHandactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={patrickHand.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={patrickHand.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={patrickHand.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={patrickHand.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
           </a>
         </div>
+
+</div>
+
       </main>
     </>
   )
 }
+
+
