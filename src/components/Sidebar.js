182b25755595f8e018b2eb0dca33fc37b07ba441
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-04-01 10:54:40 -0700

    Wrap `Link` tags in list items

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,54 +3,66 @@ import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
+    <nav className="sidebar">
+      <ul>
 
-      <Link href='/'>
-        <Image
-          src="/icons/cue.svg"
-          alt="Cue"
-          width={55}
-          height={55}
-        />
-      </Link>
+        <li>
+          <Link href='/'>
+            <Image
+              src="/icons/Cue.svg"
+              alt="Cue"
+              width={55}
+              height={55}
+            />
+          </Link>
+        </li>
 
-      <Link href='/review'>
-        <Image
-          src="/icons/Review.svg"
-          alt="Review"
-          width={55}
-          height={55}
-        />
-      </Link>
+        <li>
+          <Link href='/review'>
+            <Image
+              src="/icons/Review.svg"
+              alt="Review"
+              width={55}
+              height={55}
+            />
+          </Link>
+        </li>
 
-      <Link href='/help'>
-        <Image
-          src="/icons/Help.svg"
-          alt="Help"
-          width={55}
-          height={55}
-        />
-      </Link>
+        <li>
+          <Link href='/help'>
+            <Image
+              src="/icons/Help.svg"
+              alt="Help"
+              width={55}
+              height={55}
+            />
+          </Link>
+        </li>
 
-      <Link href='/feedback'>
-        <Image
-          src="/icons/Feedback.svg"
-          alt="Feedback"
-          width={55}
-          height={55}
-        />
-      </Link>
+        <li>
+          <Link href='/feedback'>
+            <Image
+              src="/icons/Feedback.svg"
+              alt="Feedback"
+              width={55}
+              height={55}
+            />
+          </Link>
+        </li>
 
-      <Link href='/profile'>
-        <Image
-          src="/icons/Profile.svg"
-          alt="Profile"
-          width={55}
-          height={55}
-        />
-      </Link>
+        <li>
+          <Link href='/profile'>
+            <Image
+              src="/icons/Profile.svg"
+              alt="Profile"
+              width={55}
+              height={55}
+            />
+          </Link>
+        </li>
 
-    </div>
+      </ul>
+    </nav>
   );
 };
 
