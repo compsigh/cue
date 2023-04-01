9994df8a03985180659bb961f85006e48c44b5bb
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-04-01 15:14:05 -0700

    Conditionally render `Sidebar` on every page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { SessionProvider } from 'next-auth/react'
+import Sidebar from '@/components/Sidebar'
 import '@/styles/globals.css'
 
 export default function App({
@@ -7,6 +8,7 @@ export default function App({
 }) {
   return (
     <SessionProvider session={session}>
+      {Component.name !== 'Home' && <Sidebar />}
       <Component {...pageProps} />
     </SessionProvider>
   )
