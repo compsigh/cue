9cee7ff01ef817ca5b83ca5ee5515e0eb3b7ba29
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-04-01 21:42:51 -0700

    Add styling to `ProfileCard` component

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
+import styles from './ProfileCard.module.scss';
 
 const ProfileCard = (props) => {
   const { session, signOut } = props;
   const router = useRouter();
 
   return (
-    <div className="profile-card">
-      <div className="profile-card-image">
+    <div className={styles.profileCard}>
+      <div className={styles.profileCardImage}>
         <Image
           src={session.user.image}
           alt="Profile Picture"
@@ -15,9 +16,9 @@ const ProfileCard = (props) => {
           height={110}
         />
       </div>
-      <div className="profile-card-info">
-        <h3>{session.user.name}</h3>
-        <p>{session.user.email}</p>
+      <div className={styles.profileCardInfo}>
+        <h3 className={styles.name}>{session.user.name}</h3>
+        <p className={styles.email}>{session.user.email}</p>
       </div>
       <button className="profile-card-cues-button" onClick={() => {router.push('/saved');}}>View saved cues</button>
       <button className="profile-card-signout-button" onClick={() => signOut()}>Sign out</button>
