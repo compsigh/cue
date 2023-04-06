f949bb4daa31db6e963a3792103bcc728abc7efa
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-04-06 12:45:49 -0700

    Create user on sign-up

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,7 +1,17 @@
+// Next imports
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { getServerSession } from 'next-auth/next'
-import ProfileCard from '@/components/ProfileCard/ProfileCard';
+import { getToken } from 'next-auth/jwt';
+
+// Database imports
+import connect from '@/functions/db-connect.js';
+import User from '@/schemas/user-schema.js';
+
+// Style imports
 import styles from '@/styles/Profile.module.scss';
+
+// Component imports
+import ProfileCard from '@/components/ProfileCard/ProfileCard';
 
 export default function Profile() {
   const { data: session, status } = useSession();
@@ -26,6 +36,7 @@ export default function Profile() {
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res);
+  const token = await getToken({ req: context.req });
 
   if (!session)
     return {
@@ -34,6 +45,20 @@ export async function getServerSideProps(context) {
         permanent: false,
       }
     };
+
+  // Search the database for the user's ID
+  await connect();
+  const user = await User.findOne({ googleId: token.sub });
+
+  // If the user doesn't exist, create a new user
+  if (!user) {
+    const newUser = new User({
+      googleId: token.sub,
+      cues: [],
+      // TODO: figure out invitesRemaining based on whether the user was invited or not
+    });
+    await newUser.save();
+  }
 
   return {
     props: {
