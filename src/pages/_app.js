328dfd36a5803a33a8bead6b8f96ea2e14b7214e
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-04-08 21:27:49 -0700

    Add global font vars

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,17 @@
+// Next imports
 import { SessionProvider } from 'next-auth/react'
 import { useRouter } from 'next/router'
+
+// Component imports
 import Sidebar from '@/components/Sidebar/Sidebar'
+
+// Style imports
 import '@/styles/globals.scss'
-import { Patrick_Hand } from '@next/font/google'
+import { Patrick_Hand, Patrick_Hand_SC, Public_Sans, Roboto_Mono } from 'next/font/google'
 const patrickHand = Patrick_Hand({ subsets: ['latin'], weight: '400' });
+const patrickHandSC = Patrick_Hand_SC({ subsets: ['latin'], weight: '400' });
+const publicSans = Public_Sans({ subsets: ['latin'], style: ['normal', 'italic'] });
+const robotoMono = Roboto_Mono({ subsets: ['latin'], style: ['normal', 'italic'] });
 
 export default function App({
   Component,
@@ -15,10 +23,13 @@ export default function App({
     <>
       <style jsx global>{`
         :root {
-          --font-stack: 'Patrick Hand', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+          --font-primary: ${publicSans.style.fontFamily}, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+          --font-headers: ${patrickHand.style.fontFamily}, ${publicSans.style.fontFamily}, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+          --font-buttons: ${patrickHandSC.style.fontFamily}, ${patrickHand.style.fontFamily}, ${publicSans.style.fontFamily}, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+          --font-code: ${robotoMono.style.fontFamily}, monospace;
         }
       `}</style>
-      <main className={patrickHand.className}>
+      <main>
         <SessionProvider session={session}>
           {showSidebar && <Sidebar />}
           <Component {...pageProps} />
