73ff7cc8ed62a380a2b0228ec6d75381453ddf74
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-05-24 20:01:20 -0700

    Reformat generation response

--- src/pages/api/generate.js
+++ src/pages/api/generate.js
@@ -14,9 +14,15 @@ Keep these in mind to curate your response:
 - Only suggest questions in the context of the given notes, and not what you already know about the topic.
 - Pick only the most relevant and high-level pieces of information to convert into questions.
 - For each suggested question, generate a potential answer. These answers, like your suggested questions, should only be derived from the given notes, and not any prior knowledge of the subject.
-- Format your response as a JSON object, containing the following:
-  - A \`notes_identified\` key indicating the amount of separate notes processed. These may be formatted in the given notes as Markdown bullet points (-), newlines, or some other form of delineation. It is up to you to determine what that is.
-  - A \`cues\` key, whose value is an array of objects, each object containing two properties: \`question\`, for your suggested question, and \`answer\` for your generated potential answer to your suggested question.
+- Format your response like so, and do not deviate from it:
+$NOTES_IDENTIFIED
+(2 new lines)
+$CUE
+(2 new lines)
+$ANSWER
+- Replace $NOTES_IDENTIFIED with a numerical value indicating the amount of separate notes processed. These may be formatted in the given notes as Markdown bullet points (-), newlines, or some other form of delineation. It is up to you to determine what that is.
+- Replace $CUE with your suggested question.
+- Replace $ANSWER with your generated potential answer to your suggested question.
 
 Below are two examples of notes and relevant prompts for those notes.
 
@@ -24,13 +30,13 @@ First set of notes:
 - To access knowledge is to access power; distribution of knowledge goes hand-in-hand with distribution of power
 - Rome became a center of intellectual development
 - New knowledge can subvert the old knowledge
-  - If new knowledge is power as well, it likewise has the ability to subvert old power
+- If new knowledge is power as well, it likewise has the ability to subvert old power
 - Very learned priests were interested in figuring out the way to relate to God
 - Lawyers: humans relate to each other
 - Ruling class: steeped in mos maiorum
 - Romans were willing to adapt other people's knowledge if it brought them advantage
 - Knowledge was mainly transferred through apprenticeship or by ancestry
-  - Elites wanted to keep the knowledge in their hands
+- Elites wanted to keep the knowledge in their hands
 
 First set of active recall prompts:
 - What was the relationship between knowledge and power in Rome?
