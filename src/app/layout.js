a56f573a7fa47eb7b7d35bc3b90f420a3be83788
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-05-27 13:28:36 +0200

    Add App Router root layout
    Notable differences:
    • Metadata API
    • Extracted Auth.js `SessionProvider` to new `AuthWrapper` client component
    • Extracted Sidebar conditional rendering to component-level
    
    Known issues:
    • It's bugging out about the styles
    • There was something with the `children` not being a valid React child or something like that

---
+++ src/app/layout.js
@@ -0,0 +1,42 @@
+// Next imports
+import AuthWrapper from '../components/AuthWrapper'
+
+// Component imports
+import Sidebar from '@/components/Sidebar/Sidebar'
+
+// Style imports
+import '@/styles/globals.scss'
+import { Patrick_Hand, Patrick_Hand_SC, Public_Sans, Roboto_Mono } from 'next/font/google'
+const patrickHand = Patrick_Hand({ subsets: ['latin'], weight: '400' })
+const patrickHandSC = Patrick_Hand_SC({ subsets: ['latin'], weight: '400' })
+const publicSans = Public_Sans({ subsets: ['latin'], style: ['normal', 'italic'] })
+const robotoMono = Roboto_Mono({ subsets: ['latin'], style: ['normal', 'italic'] })
+
+export const metadata = {
+  title: 'Cue',
+  description: 'Study with AI-powered Active Recall',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <AuthWrapper>
+      <html lang="en">
+        {/* <style jsx global>{`
+          :root {
+            --font-primary: ${publicSans.style.fontFamily}, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+            --font-headers: ${patrickHand.style.fontFamily}, ${publicSans.style.fontFamily}, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+            --font-buttons: ${patrickHandSC.style.fontFamily}, ${patrickHand.style.fontFamily}, ${publicSans.style.fontFamily}, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+            --font-code: ${robotoMono.style.fontFamily}, monospace;
+          }
+          `}
+        </style> */}
+        <body>
+          <main>
+            <Sidebar />
+            {children}
+          </main>
+        </body>
+      </html>
+    </AuthWrapper>
+  )
+}
