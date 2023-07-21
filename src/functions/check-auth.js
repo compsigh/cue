5ab2c18ff03a97b0d0df8bce62c1ce1df944fbb2
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-07-21 14:29:01 -0700

    Add auth policies & logic (#11)

---
+++ src/functions/check-auth.js
@@ -0,0 +1,48 @@
+// This file details the policies for authentication,
+// and provides a function for checking if a user is authenticated based on the current policy.
+
+// The policies are sorted from most to least restrictive.
+// Each consecutive step opens up Cue to more users,
+// and those who match previous steps can still access the app.
+
+// The policies are:
+// 1. `usf-only` — Only users with a USF email address can access Cue
+// 2. `invite-only` — Only users with a valid invite code can access Cue
+// 3. `edu-only` — Only users with an email address from an educational institution can access Cue
+// 4. `public` — Users with any email address can sign up for Cue
+
+// Example user:                  edward@dons.usfca.edu | invited.student@gmail.com | rohan@sjsu.edu | student@gmail.com
+// Can access with auth levels:   1, 2, 3, 4            | 2, 3, 4                   | 3, 4           | 4
+
+export const authLevels = {
+  'usf-only': 1,
+  'invite-only': 2,
+  'edu-only': 3,
+  'public': 4
+}
+export const currentPolicy = 'invite-only'
+export const currentAuthLevel = authLevels[currentPolicy]
+
+export default async function checkAuth (currentAuthLevel, user) {
+  if (!user)
+    return false
+
+  if (currentAuthLevel >= 1) {
+    const email = user.email.toLowerCase()
+    if (email.endsWith('@dons.usfca.edu'))
+      return true
+  }
+
+  if (currentAuthLevel >= 2) {
+    // TODO: invite code logic
+  }
+
+  if (currentAuthLevel >= 3) {
+    const email = user.email.toLowerCase()
+    if (email.endsWith('.edu'))
+      return true
+  }
+
+  if (currentAuthLevel >= 4)
+    return true
+}
