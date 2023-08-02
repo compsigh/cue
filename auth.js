d9581feb71f457a6c611fe57d1ab60ded5b6de28
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-08-02 10:46:53 -0700

    Upgrade configuration

---
+++ auth.js
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile (profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async session ({ session, token }) {
+      session.user.id = token.sub
+      return session
+    }
+  }
+}
+
+export const { handlers: { GET, POST }, auth } = NextAuth(authOptions)
