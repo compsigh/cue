275fc64df68a9db1e45ea0231386671c6b917aba
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-08-12 15:58:49 -0700

    Make `invite-only` auth policy more restrictive
    This lines up with reality: the amount of students that can have invites < the amount of USF students

--- src/functions/check-auth.js
+++ src/functions/check-auth.js
@@ -6,22 +6,22 @@
 // and those who match previous steps can still access the app.
 
 // The policies are:
-// 1. `usf-only` — Only users with a USF email address can access Cue
-// 2. `invite-only` — Only users with a valid invite code can access Cue
-// 3. `edu-only` — Only users with an email address from an educational institution can access Cue
-// 4. `public` — Users with any email address can sign up for Cue
+// 1. `invite-only` — Only users with a valid invite code can access Cue
+// 2. `usf-only` — Users with a USF email address or a valid invite code can access Cue
+// 3. `edu-only` — Users with an email address from an educational institution or a valid invite code can access Cue
+// 4. `public` — Users with any email address can access Cue
 
-// Example user:                  edward@dons.usfca.edu | invited.student@gmail.com | rohan@sjsu.edu | student@gmail.com
-// Can access with auth levels:   1, 2, 3, 4            | 2, 3, 4                   | 3, 4           | 4
+// Example user:                  invited.student@gmail.com | student@dons.usfca.edu | student@berkeley.edu | student@gmail.com
+// Can access with auth levels:   1, 2, 3, 4                | 2, 3, 4                | 3, 4                 | 4
 
 import { createUser } from './user-management'
 import { validate } from './invite-management'
 
 export const authLevels = {
-  'usf-only': 1,
-  'invite-only': 2,
+  'invite-only': 1,
+  'usf-only': 2,
   'edu-only': 3,
-  'public': 4
+  public: 4
 }
 export const currentPolicy = 'invite-only'
 export const currentAuthLevel = authLevels[currentPolicy]
@@ -30,15 +30,7 @@ export default async function checkAuth ({ user, inviteCode }) {
   if (!user.sessionData)
     return false
 
-  if (currentAuthLevel >= 1) {
-    const email = user.sessionData.email.toLowerCase()
-    if (email.endsWith('@dons.usfca.edu')) {
-      await createUser()
-      return true
-    }
-  }
-
-  if (currentAuthLevel >= 2)
+  if (currentAuthLevel >= 1)
     if (inviteCode) {
       const invite = await validate(inviteCode)
       if (invite?.valid) {
@@ -49,6 +41,14 @@ export default async function checkAuth ({ user, inviteCode }) {
         return true
       }
     }
+
+  if (currentAuthLevel >= 2) {
+    const email = user.sessionData.email.toLowerCase()
+    if (email.endsWith('@dons.usfca.edu')) {
+      await createUser()
+      return true
+    }
+  }
 
   if (currentAuthLevel >= 3) {
     const email = user.sessionData.email.toLowerCase()
