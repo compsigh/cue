15ad7301e06ed7e89932787b3a45feb93ac0fe57
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-01-14 18:57:15 -0800

    Add TypeScript config
    Generated by Next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true
+  }
 }
 
 module.exports = {
